1e97877e7480fcf22293344afbce04c2113cb6fe
antonia <antonia@unifesspa.edu.br>
2020-11-03 17:05:56 -0300

    Criacao do controller meedico

--- src/Controller/MedicoController.php
+++ src/Controller/MedicoController.php
@@ -5,13 +5,24 @@ namespace App\Controller;
 
 
 use App\Entity\Medico;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MedicoController
+class MedicoController extends AbstractController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/medicos", methods={"POST"})
      */
@@ -25,6 +36,36 @@ class MedicoController
         $medico->crm = $dadosEmJson->crm;
         $medico->nome = $dadosEmJson->nome;
 
+        $this->entityManager->persist($medico);
+        $this->entityManager->flush();
+
         return new JsonResponse($medico);
-}
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={GET})
+     */
+    public function findAll(): Response
+    {
+        respositoryMedicos = $this
+            ->getDoctrine()
+            ->getRepository(Medico::class);
+
+        $medicoList = $repositoryDeMedicos->findAll();
+        return new JsonResponse($medicoList);
+    }
+
+    public function findOne(Request $request): Response
+    {
+        $id = $request->get('id');
+        ->
+        $this->getDoctrine()
+            ->getRepository(Medico::class);
+        $medico = $repositoryMedicos->find($id);
+        $codigoRetorno = is_null($medico) ? Response::HTTP_NO_CONTENT : 200;
+
+        return new JsonResponse($medico, $codigoRetorno);
+
+    }
+
 }
